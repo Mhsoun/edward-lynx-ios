70a107f54e0ce38d4fea3afcbd283d64edfa6640
Jason Jon E. Carreos <jason@ingenuity.ph>
2016-12-12 18:14:27 +0800

    Surveys API client

---
+++ EdwardLynx/API/ELSurveysAPIClient.h
@@ -0,0 +1,21 @@
+//
+//  ELSurveysAPIClient.h
+//  EdwardLynx
+//
+//  Created by Jason Jon E. Carreos on 12/12/2016.
+//  Copyright © 2016 Ingenuity Global Consulting. All rights reserved.
+//
+
+#import "ELAPIClient.h"
+
+@interface ELSurveysAPIClient : ELAPIClient
+
+- (void)createUserSurveyWithParams:(NSDictionary *)params
+                        completion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion;
+- (void)currentUserSurveysWithCompletion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion;
+- (void)updateUserSurveyWithParams:(NSDictionary *)params
+                        completion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion;
+- (void)userSurveyWithId:(int64_t)surveyId
+              completion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion;
+
+@end
---
+++ EdwardLynx/API/ELSurveysAPIClient.m
@@ -0,0 +1,54 @@
+//
+//  ELSurveysAPIClient.m
+//  EdwardLynx
+//
+//  Created by Jason Jon E. Carreos on 12/12/2016.
+//  Copyright © 2016 Ingenuity Global Consulting. All rights reserved.
+//
+
+#import "ELSurveysAPIClient.h"
+
+@implementation ELSurveysAPIClient
+
+- (void)createUserSurveyWithParams:(NSDictionary *)params
+                        completion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion {
+    NSMutableURLRequest *request = [super requestFor:kELAPISurveysEndpoint
+                                              method:kELAPIPostHTTPMethod
+                                          bodyParams:params];
+    
+    [super performAuthenticatedTask:YES
+                        withRequest:request
+                         completion:completion];
+}
+
+- (void)currentUserSurveysWithCompletion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion {
+    NSMutableURLRequest *request = [super requestFor:kELAPISurveysEndpoint
+                                              method:kELAPIGetHTTPMethod];
+    
+    [super performAuthenticatedTask:YES
+                        withRequest:request
+                         completion:completion];
+}
+
+- (void)updateUserSurveyWithParams:(NSDictionary *)params
+                        completion:(void (^)(NSURLResponse *, NSDictionary *, NSError *))completion {
+    NSMutableURLRequest *request = [super requestFor:kELAPISurveysEndpoint
+                                              method:kELAPIPatchHTTPMethod
+                                          bodyParams:params];
+    
+    [super performAuthenticatedTask:YES
+                        withRequest:request
+                         completion:completion];
+}
+
+- (void)userSurveyWithId:(int64_t)surveyId
+              completion:(void (^)(NSURLResponse *response, NSDictionary *responseDict, NSError *error))completion {
+    NSMutableURLRequest *request = [super requestFor:[NSString stringWithFormat:kELAPISurveyDetailsEndpoint, @(surveyId)]
+                                              method:kELAPIGetHTTPMethod];
+    
+    [super performAuthenticatedTask:YES
+                        withRequest:request
+                         completion:completion];
+}
+
+@end
