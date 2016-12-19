388e7347a5cfbe184a2a362099dd0c13448b8002
Jason Jon E. Carreos <jason@ingenuity.ph>
2016-12-19 13:53:46 +0800

    Initial Left Menu Navigation setup

---
+++ EdwardLynx/Controllers/ELLeftMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELLeftMenuViewController.h
+//  EdwardLynx
+//
+//  Created by Jason Jon E. Carreos on 19/12/2016.
+//  Copyright © 2016 Ingenuity Global Consulting. All rights reserved.
+//
+
+#import "ELBaseViewController.h"
+
+@interface ELLeftMenuViewController : ELBaseViewController
+
+@end
