64b0b1e24d2ec79a25721f90972932e4279fef9c
Jason Jon E. Carreos <jason@ingenuity.ph>
2017-03-21 12:20:57 +0800

    Created Notifications view

---
+++ EdwardLynx/Subclasses/Views/Main/ELNotificationsView.m
@@ -0,0 +1,20 @@
+//
+//  ELNotificationsView.m
+//  EdwardLynx
+//
+//  Created by Jason Jon E. Carreos on 21/03/2017.
+//  Copyright © 2017 Ingenuity Global Consulting. All rights reserved.
+//
+
+#import "ELNotificationsView.h"
+
+@implementation ELNotificationsView
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    // UI
+    self.layer.cornerRadius = 5.0f;
+}
+
+@end
