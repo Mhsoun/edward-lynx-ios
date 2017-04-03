0814302337b9f3cf5e939a8883a4c66fd7265d6f
Jason Jon E. Carreos <jason@ingenuity.ph>
2017-04-03 11:22:32 +0800

    Updates to dashboard

--- EdwardLynx/Controllers/Main/ELDashboardViewController.m
+++ EdwardLynx/Controllers/Main/ELDashboardViewController.m
@@ -8,21 +8,16 @@
 
 #import "ELDashboardViewController.h"
 #import "AppDelegate.h"
-#import "ELActionView.h"
 #import "ELDashboardHeaderTableViewCell.h"
 #import "ELDashboardReminderTableViewCell.h"
 #import "ELDevelopmentPlanTableViewCell.h"
 #import "ELDevelopmentPlanDetailsViewController.h"
-#import "ELSectionView.h"
-#import "ELShortcutView.h"
-#import "ELStatusView.h"
-#import "ELTabPageViewController.h"
-
 #import "ELNotificationView.h"
+#import "ELSectionView.h"
+#import "ELTabPageViewController.h"
 
 #pragma mark - Private Constants
 
-//static CGFloat const kELCornerRadius = 5.0f;
 static NSString * const kELHeaderCellIdentifier = @"DashboardHeaderCell";
 static NSString * const kELDevPlanCellIdentifier = @"DevelopmentPlanCell";
 static NSString * const kELReminderCellIdentifier = @"DashboardReminderCell";
@@ -182,125 +177,6 @@ static NSString * const kELReminderCellIdentifier = @"DashboardReminderCell";
     sectionView.delegate = self;
     
     return sectionView;
-}
-
-#pragma mark - Protocol Methods (ELBaseViewController)
-
-- (void)layoutPage {
-//    ELActionView *actionView;
-//    ELStatusView *statusView;
-//    ELShortcutView *shortcutView;
-//    
-//    // Status section
-//    statusView = [[ELStatusView alloc] initWithDetails:@{@"title": @"Development Plan Status",
-//                                                         @"segue": @"",
-//                                                         @"details": @"3/4 goals completed",
-//                                                         @"permissions": @[@(kELRolePermissionCreateDevelopmentPlan)]}];
-//    statusView.frame = self.devPlanStatusView.bounds;
-////    statusView.delegate = self;
-//    
-//    [self.devPlanStatusView addSubview:statusView];
-//    [self.devPlanStatusView.layer setCornerRadius:kELCornerRadius];
-//    
-//    statusView = [[ELStatusView alloc] initWithDetails:@{@"title": @"Feedback request status",
-//                                                         @"segue": @"",
-//                                                         @"details": @"3/4 submitted results",
-//                                                         @"permissions": @[@(kELRolePermissionParticipateInSurvey),
-//                                                                           @(kELRolePermissionSubmitSurvey),
-//                                                                           @(kELRolePermissionInstantFeedback)]}];
-//    statusView.frame = self.feedbackStatusView.bounds;
-////    statusView.delegate = self;
-//    
-//    [self.feedbackStatusView addSubview:statusView];
-//    [self.feedbackStatusView.layer setCornerRadius:kELCornerRadius];
-//    
-//    // Shortcuts section
-//    shortcutView = [[ELShortcutView alloc] initWithDetails:@{@"title": @"Create Instant Feedback",
-//                                                             @"segue": @"CreateInstantFeedback",
-//                                                             @"description": @"Description on creating instant feedback.",
-//                                                             @"permissions": @[@(kELRolePermissionInstantFeedback)]}];
-//    shortcutView.frame = self.createFeedbackView.bounds;
-//    shortcutView.delegate = self;
-//    
-//    [self.createFeedbackView addSubview:shortcutView];
-//    [self.createFeedbackView.layer setCornerRadius:kELCornerRadius];
-//    
-//    shortcutView = [[ELShortcutView alloc] initWithDetails:@{@"title": @"Create Development Plan",
-//                                                             @"segue": @"CreateDevelopmentPlan",
-//                                                             @"description": @"Description on creating development plan.",
-//                                                             @"permissions": @[@(kELRolePermissionCreateDevelopmentPlan)]}];
-//    shortcutView.frame = self.createDevPlanView.bounds;
-//    shortcutView.delegate = self;
-//    
-//    [self.createDevPlanView addSubview:shortcutView];
-//    [self.createDevPlanView.layer setCornerRadius:kELCornerRadius];
-//    
-//    shortcutView = [[ELShortcutView alloc] initWithDetails:@{@"title": @"View Reports",
-//                                                             @"segue": @"Report",
-//                                                             @"description": @"Description on viewing reports.",
-//                                                             @"permissions": @[@(kELRolePermissionViewAnonymousIndividualReports),
-//                                                                               @(kELRolePermissionViewAnonymousTeamReports)]}];
-//    shortcutView.frame = self.reportsView.bounds;
-//    shortcutView.delegate = self;
-//    
-//    [self.reportsView addSubview:shortcutView];
-//    [self.reportsView.layer setCornerRadius:kELCornerRadius];
-//    
-//    shortcutView = [[ELShortcutView alloc] initWithDetails:@{@"title": @"View Surveys",
-//                                                             @"segue": @"Survey",
-//                                                             @"description": @"Description on viewing surveys.",
-//                                                             @"permissions": @[@(kELRolePermissionParticipateInSurvey)]}];
-//    shortcutView.frame = self.surveysView.bounds;
-//    shortcutView.delegate = self;
-//    
-//    [self.surveysView addSubview:shortcutView];
-//    [self.surveysView.layer setCornerRadius:kELCornerRadius];
-//    
-//    // Action Required section
-//    actionView = [[ELActionView alloc] initWithDetails:@{@"value": @"360",
-//                                                         @"title": @"Feedback Requests",
-//                                                         @"count": @(0),
-//                                                         @"segue": @"",
-//                                                         @"color": kELGreenColor,
-//                                                         @"permissions": @[@(kELRolePermissionParticipateInSurvey),
-//                                                                           @(kELRolePermissionSubmitSurvey)]}];
-//    actionView.frame = self.feedbackActionView.bounds;
-////    actionView.delegate = self;
-//    
-//    [self.feedbackActionView addSubview:actionView];
-//    [self.feedbackActionView.layer setCornerRadius:kELCornerRadius];
-//    
-//    actionView = [[ELActionView alloc] initWithDetails:@{@"value": @"New",
-//                                                         @"title": @"Reports",
-//                                                         @"count": @(0),
-//                                                         @"segue": @"",
-//                                                         @"color": kELBlueColor,
-//                                                         @"permissions": @[@(kELRolePermissionViewAnonymousIndividualReports),
-//                                                                           @(kELRolePermissionViewAnonymousTeamReports)]}];
-//    actionView.frame = self.reportsActionView.bounds;
-////    actionView.delegate = self;
-//    
-//    [self.reportsActionView addSubview:actionView];
-//    [self.reportsActionView.layer setCornerRadius:kELCornerRadius];
-//    
-//    actionView = [[ELActionView alloc] initWithDetails:@{@"value": @"Instant",
-//                                                         @"title": @"Feedback Requests",
-//                                                         @"count": @(0),
-//                                                         @"segue": @"InstantFeedback",
-//                                                         @"color": kELPinkColor,
-//                                                         @"permissions": @[@(kELRolePermissionInstantFeedback)]}];
-//    actionView.frame = self.instantFeedbackActionView.bounds;
-//    actionView.delegate = self;
-//    
-//    [self.instantFeedbackActionView addSubview:actionView];
-//    [self.instantFeedbackActionView.layer setCornerRadius:kELCornerRadius];
-    
-    // Navigation Bar
-//    self.navigationController.navigationBar.shadowImage = [UIImage new];
-//    self.navigationController.navigationBar.translucent = YES;
-//    
-//    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
-//                                                  forBarMetrics:UIBarMetricsDefault];
 }
 
 #pragma mark - Protocol Methods (ELDashboardViewDelegate)
