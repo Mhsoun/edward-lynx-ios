ea267e18005f436c441d8a1c2450eb98c6c89dcc
Jason Jon E. Carreos <jason@ingenuity.ph>
2017-04-05 18:20:26 +0800

    Subclass UILabel for padding

---
+++ EdwardLynx/Subclasses/Others/ELLabel.h
@@ -0,0 +1,13 @@
+//
+//  ELLabel.h
+//  EdwardLynx
+//
+//  Created by Jason Jon E. Carreos on 05/04/2017.
+//  Copyright © 2017 Ingenuity Global Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELLabel : UILabel
+
+@end
