6562b84d5c46cbd17d360e737554ecce41ff7750
Jason Jon E. Carreos <jason@ingenuity.ph>
2017-06-22 13:16:10 +0800

    Reworked Manager Individual cell to user Collection view

--- EdwardLynx/Subclasses/Cells/Manager/ELCircleChartCollectionViewCell.m
+++ EdwardLynx/Subclasses/Cells/Manager/ELCircleChartCollectionViewCell.m
@@ -1,65 +1,37 @@
 //
-//  ELCircleChart.m
+//  ELCircleChartCollectionViewCell.m
 //  EdwardLynx
 //
-//  Created by Jason Jon E. Carreos on 20/06/2017.
+//  Created by Jason Jon E. Carreos on 21/06/2017.
 //  Copyright © 2017 Ingenuity Global Consulting. All rights reserved.
 //
 
-#import "ELCircleChart.h"
+#import <PNChart/PNChart.h>
 
-#pragma mark - Class Extension
+#import "ELCircleChartCollectionViewCell.h"
 
-@interface ELCircleChart ()
+@interface ELCircleChartCollectionViewCell ()
 
 @property (nonatomic, strong) NSDictionary *detailsDict;
 @property (nonatomic, strong) PNCircleChart *circleChart;
 
 @end
 
-@implementation ELCircleChart
-
-#pragma mark - Lifecycle
-
-- (id)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    
-    if (!self) {
-        return nil;
-    }
-    
-    [self customInit];
-    
-    return self;
-}
-
-- (id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    
-    if (!self) {
-        return nil;
-    }
-    
-    [self customInit];
-    
-    return self;
-}
+@implementation ELCircleChartCollectionViewCell
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     
+    // Update content
     [self.chartView layoutIfNeeded];
-    [self setupChartView];
+    [self setupContent];
 }
 
-- (void)customInit {
-    [[NSBundle mainBundle] loadNibNamed:@"CircleChart" owner:self options:nil];
-    [self addSubview:self.view];
-    
-    self.view.frame = self.bounds;
+- (void)configure:(id)object atIndexPath:(NSIndexPath *)indexPath {
+    self.detailsDict = (NSDictionary *)object;
 }
 
-- (void)setupChartView {
+- (void)setupContent {
     self.nameLabel.text = self.detailsDict[@"name"];
     self.circleChart = [[PNCircleChart alloc] initWithFrame:self.chartView.bounds
                                                       total:[NSNumber numberWithInt:100]
@@ -72,15 +44,12 @@
     
     [ELUtils circleChart:self.circleChart progress:[self.detailsDict[@"percentage"] floatValue]];
     
-    self.circleChart.countingLabel.font = Font(@"Lato-Bold", 10.0f);
+    self.circleChart.countingLabel.font = Font(@"Lato-Bold", 12.0f);
     
     [self.circleChart setDisplayAnimated:NO];
     [self.circleChart strokeChart];
+    
     [self.chartView addSubview:self.circleChart];
-}
-
-- (void)setupContent:(NSDictionary *)detailsDict {
-    self.detailsDict = detailsDict;
 }
 
 @end
--- EdwardLynx/Subclasses/Cells/Manager/ELManagerIndividualTableViewCell.m
+++ EdwardLynx/Subclasses/Cells/Manager/ELManagerIndividualTableViewCell.m
@@ -7,7 +7,20 @@
 //
 
 #import "ELManagerIndividualTableViewCell.h"
-#import "ELCircleChart.h"
+#import "ELCircleChartCollectionViewCell.h"
+
+#pragma mark - Private Constants
+
+static CGFloat const kELSpacing = 5;
+static NSString * const kELCellIdentifier = @"CircleChartCell";
+
+#pragma mark - Class Extension
+
+@interface ELManagerIndividualTableViewCell ()
+
+@property (nonatomic, strong) NSArray *devPlans;
+
+@end
 
 @implementation ELManagerIndividualTableViewCell
 
@@ -16,6 +29,13 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    
+    // Initialization
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [self.collectionView registerNib:[UINib nibWithNibName:kELCellIdentifier bundle:nil]
+          forCellWithReuseIdentifier:kELCellIdentifier];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -25,21 +45,46 @@
 }
 
 - (void)configure:(id)object atIndexPath:(NSIndexPath *)indexPath {
-    UITapGestureRecognizer *tap;
     NSDictionary *individualDict = (NSDictionary *)object;
-    NSArray *items = individualDict[@"data"];
     
     self.nameLabel.text = individualDict[@"name"];
+    self.devPlans = individualDict[@"data"];
     
-    tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onChartSelection:)];
+    [self.collectionView reloadData];
+}
+
+#pragma mark - Protocol Methods (UICollectionView)
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.devPlans.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    ELCircleChartCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kELCellIdentifier
+                                                                                      forIndexPath:indexPath];
     
-    for (int i = 0; i < items.count; i++) {
-        ELCircleChart *chartView = self.chartViews[i];
-        
-        [chartView setHidden:NO];
-        [chartView setupContent:items[i]];
-        [chartView addGestureRecognizer:tap];
-    }
+    [cell configure:self.devPlans[indexPath.row] atIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake((CGRectGetWidth(collectionView.frame) / 3) - kELSpacing, 110);
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    // NOTE Disable for now
+//    [self.delegate onChartSelection:nil];  // TODO Should be instance of dev plan
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return kELSpacing;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return kELSpacing;
 }
 
 #pragma mark - Interface Builder Actions
@@ -47,13 +92,6 @@
 - (IBAction)onSeeMoreButtonClick:(id)sender {
     // NOTE Disable for now
 //    [self.delegate onSeeMore:nil];  // TODO Should be id of dev plan selected
-}
-
-#pragma mark - Selectors
-
-- (void)onChartSelection:(UIGestureRecognizer *)recognizer {
-    // NOTE Disable for now
-//    [self.delegate onChartSelection:nil];  // TODO Should be instance of dev plan
 }
 
 @end
